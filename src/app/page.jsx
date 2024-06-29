ff9c2f3a20959d8cb2f3ab9bca8fed3d11a6d4c7
Lucas Santana <lucas_377@hotmail.com>
2024-06-29 15:50:07 -0300

    feat: Pagination
    - Updated fetch to render n items and start pagination for the posts

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,9 @@ import logger from "@/logger";
  * @returns JSON
  */
 async function getAllPosts() {
-  const response = await fetch("http://localhost:3042/posts");
+  const response = await fetch(
+    "http://localhost:3042/posts?_page=1&_per_page=4"
+  );
 
   if (!response.ok) {
     logger.error("Erro na obtenção dos posts.");
@@ -23,7 +25,7 @@ export default async function Home() {
 
   return (
     <main className={styles.main}>
-      {posts.map((post) => (
+      {posts.data.map((post) => (
         <Card post={post} />
       ))}
     </main>
