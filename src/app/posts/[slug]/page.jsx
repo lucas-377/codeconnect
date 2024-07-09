e5071b0acafcc7ecd1cd271b088b76e0cc9b3df7
Lucas Santana <lucas_377@hotmail.com>
2024-07-08 22:43:41 -0300

    feat: Post
    - Installed libs to handle markdown to HTML

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -1,4 +1,6 @@
 import logger from "@/logger";
+import { remark } from "remark";
+import html from "remark-html";
 
 /**
  * Return one post from backend
@@ -22,11 +24,24 @@ async function getPostBySlug(slug) {
     return {};
   }
 
-  return data[0];
+  const post = data[0];
+
+  // Format markdown content to render as HTML
+  const processedContent = await remark().use(html).process(post.markdown);
+  const contentHtml = processedContent.toString();
+
+  post.markdown = contentHtml;
+
+  return post;
 }
 
 export default async function PostPage({ params }) {
   const data = await getPostBySlug(params.slug);
 
-  return <h1>{data.title}</h1>;
+  return (
+    <>
+      <h1>{data.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: data.markdown }} />
+    </>
+  );
 }
